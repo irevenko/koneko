9d4478886f6a96ee01b28da9e4017c1b9c504ee9
irevenko <kekalkaru20@gmail.com>
2021-04-02 22:29:54 +0300

    Update main.go

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # 🐈🌐 koneko
  Terminal client for nyaa.si. Download Anime, Manga, Music and more
+
+
+# ToDo
+- [ ] command nyaa && sukebei (to select provider)
+- [ ] Search
+- [ ] Bookmarks
+- [ ] History
+- [ ] Help
+- [ ] Download torrent or open in other app (mark torrents like in gero)
+- [ ] Simulate nyaa.si as much as possible
+    * [ ] Colors and theme
+    * [ ] Torrent description, comments, files
--- main.go
+++ main.go
@@ -1,25 +1,73 @@
 package main
 
 import (
+	"fmt"
+	"strconv"
 	"strings"
 
 	"github.com/gdamore/tcell/v2"
 	"github.com/rivo/tview"
 )
 
+var app = tview.NewApplication()
+
 const tableHeaders = `Category|Name|Link|Size|Date|Seeders|Leechers|Downloads`
 
 const fakeData = tableHeaders +
 	`
-Anime|Kimetsu no yaiba|https://nyaa.si/download/1232132121|20MiB|11.21.2010|95|42|189
-Literature|Naruto manga|https://nyaa.si/download/9098086551|1GiB|22.01.2022|34|97|300
-Software|Doki Doki lit club|https://nyaa.si/download/1596796751|23MiB|06.03.2015|66|2|19`
-
-var app = tview.NewApplication()
+Anime|Kimetsu no yaiba|https://nyaa.si/download/321|20MiB|11.21.2010|95|42|189
+Literature|Naruto manga|https://nyaa.si/download/3321|1GiB|22.01.2022|34|97|300
+Software|Doki Doki lit club|https://nyaa.si/22221|23MiB|06.03.2015|66|2|19`
 
 var query = ""
 var category = ""
 var filter = ""
+var sortBy = ""
+
+var nyaaCategories = []string{
+	"All",
+	"Anime",
+	"- Anime Music Video",
+	"- English-translated",
+	"- Non-English-translated",
+	"- Raw",
+	"Audio",
+	"- Lossless",
+	"- Lossy",
+	"Literature",
+	"- English-translated",
+	"- Non-English-translated",
+	"- Raw",
+	"Live Action",
+	"- English-translated",
+	"- Idol/Promotional Video",
+	"- Non-English-translated",
+	"- Raw",
+	"Pictures",
+	"- Graphics",
+	"- Photos",
+	"Software",
+	"- Applications",
+	"- Games",
+}
+
+var filters = []string{
+	"No filter",
+	"No remakes",
+	"Trusted only",
+}
+
+var sortOptions = []string{
+	"Downloads",
+	"Comments",
+	"Date",
+	"Seeders",
+	"Leechers",
+	"Size",
+}
+
+const torrentText = `Description: lol
+Comments: xd`
 
 func main() {
 	table := tview.NewTable().
@@ -29,48 +77,55 @@ func main() {
 	table.SetBorder(true).SetTitle("nyaa.si")
 
 	form := tview.NewForm().
-		AddInputField("Search", "", 25, nil, func(text string) {
+		AddInputField("Query", "", 24, nil, func(text string) {
 			query = text
 		}).
-		AddDropDown("Category", []string{
-			"All",
-			"Anime",
-			"- Anime Music Video",
-			"- English-translated",
-			"- Non-English-translated",
-			"- Raw",
-			"Audio",
-			"- Lossless",
-			"- Lossy",
-			"Literature",
-			"- English-translated",
-			"- Non-English-translated",
-			"- Raw",
-			"Live Action",
-			"- English-translated",
-			"- Idol/Promotional Video",
-			"- Non-English-translated",
-			"- Raw",
-			"Pictures",
-			"- Graphics",
-			"- Photos",
-			"Software",
-			"- Applications",
-			"- Games",
-		}, 0, func(option string, optionIndex int) {
+		AddDropDown("Category", nyaaCategories, 0, func(option string, optionIndex int) {
 			category = option
 		}).
-		AddDropDown("Filter", []string{
-			"No filter",
-			"No remakes",
-			"Trusted only",
-		}, 0, func(option string, optionIndex int) {
+		AddDropDown("Filter", filters, 0, func(option string, optionIndex int) {
 			filter = option
+		}).
+		AddDropDown("Sort By", sortOptions, 0, func(option string, optionIndex int) {
+			sortBy = option
 		}).
 		AddButton("Search", func() {
 			setTableData(table, fakeData)
 			app.SetFocus(table)
 		})
+
+	textView := tview.NewTextView().
+		SetDynamicColors(true).
+		SetRegions(true).
+		SetWordWrap(true).
+		SetChangedFunc(func() {
+			app.Draw()
+		})
+	numSelections := 0
+
+	fmt.Fprintf(textView, "%s ", torrentText)
+	textView.SetDoneFunc(func(key tcell.Key) {
+		currentSelection := textView.GetHighlights()
+		if key == tcell.KeyEnter {
+			if len(currentSelection) > 0 {
+				textView.Highlight()
+			} else {
+				textView.Highlight("0").ScrollToHighlight()
+			}
+		} else if len(currentSelection) > 0 {
+			index, _ := strconv.Atoi(currentSelection[0])
+			if key == tcell.KeyTab {
+				index = (index + 1) % numSelections
+			} else if key == tcell.KeyBacktab {
+				index = (index - 1 + numSelections) % numSelections
+			} else {
+				return
+			}
+			textView.Highlight(strconv.Itoa(index)).ScrollToHighlight()
+		}
+	})
+	textView.SetBorder(true)
+	textView.SetTitle("Torrent Info")
 
 	form.SetCancelFunc(func() {
 		app.SetFocus(table)
@@ -84,12 +139,20 @@ func main() {
 		app.SetFocus(form)
 	})
 
-	flex := tview.NewFlex().
-		AddItem(form, 0, 1, true).SetDirection(tview.FlexColumn).
-		AddItem(table, 0, 3, true).SetDirection(tview.FlexColumn)
+	table.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
+		if event.Key() == tcell.KeyCtrlD {
+			app.SetFocus(textView)
+		}
+		return event
+	})
 
-	app.SetRoot(flex, true)
-	app.EnableMouse(true)
+	flex := tview.NewFlex().
+		AddItem(form, 0, 1, true).
+		AddItem(tview.NewFlex().SetDirection(tview.FlexRow).
+			AddItem(textView, 0, 1, true).
+			AddItem(table, 0, 2, true), 0, 4, true)
+
+	app.SetRoot(flex, true).EnableMouse(true)
 	app.Run()
 }
 
